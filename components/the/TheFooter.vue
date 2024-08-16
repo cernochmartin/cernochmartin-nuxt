154c3c088be949fa92942906e6fb123f5cd65027
cernochmartin <cernochm@outlook.cz>
2024-08-16 09:53:56 +0200

    fix(TheFooter)

--- components/the/TheFooter.vue
+++ components/the/TheFooter.vue
@@ -3,7 +3,7 @@ const year = new Date().getFullYear()
 const meta = useSiteMeta()
 </script>
 <template>
-    <footer class="h-12 flex flex-col gap-4 items-center justify-center text-footer fixed w-full pb-12">
+    <footer class="h-12 flex flex-col gap-4 items-center justify-center text-footer fixed w-full bottom-6">
         <p class="uppercase flex justify-between w-[240px]">
             <NuxtLink to="/contact" class="animation-underline">Contact</NuxtLink>
             <NuxtLink to="/references" class="animation-underline">References</NuxtLink>
