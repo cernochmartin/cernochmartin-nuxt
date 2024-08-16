c87197c346a05c64a53ca3eb1e20fedbef628709
cernochmartin <cernochm@outlook.cz>
2024-08-16 18:33:03 +0200

    feat(BaseButton): update BaseButton component

--- components/base/BaseButton.vue
+++ components/base/BaseButton.vue
@@ -1,20 +1,38 @@
 <script setup lang="ts">
 const props = defineProps<{
-    fullBg: boolean
     text: string
-    link?: boolean
-    path?: string
+    path: string
+    icon: boolean
+    animation: boolean
+    type: 'full' | 'outline'
 }>()
 </script>
 <template>
-    <template v-if="props.fullBg">
-        <button class="bg-blue-secondary rounded-lg text-white px-4 py-1">
-            <NuxtLink :to="props.path">{{ props.text }}</NuxtLink>
-        </button>
-    </template>
-    <template v-else-if="!props.fullBg">
-        <button class="border-2 border-blue-secondary rounded-lg px-4 py-1">
-            <NuxtLink :to="props.path">{{ props.text }}</NuxtLink>
-        </button>
-    </template>
+    <NuxtLink :to="props.path" :class="type === 'full' ? 'full' : 'outline'"
+        class="px-2.5 text-white text-center py-1.5 rounded-md text-2xl animation-up">
+        {{ props.text }} <i v-if="props.icon" class="fas fa-arrow-right" />
+    </NuxtLink>
 </template>
+<style scoped>
+.full {
+    background-color: #3559c7;
+    color: #ffffff;
+}
+
+.outline {
+    background-color: transparent;
+    border: 0.5px solid #3559c7;
+    color: #3559c7;
+}
+
+.dark-mode .full {
+    background-color: #00a3ff;
+    color: #ffffff;
+}
+
+.dark-mode .outline {
+    background-color: transparent;
+    border: 0.5px solid #00a3ff;
+    color: #00a3ff;
+}
+</style>
